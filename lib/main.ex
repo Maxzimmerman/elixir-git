74d3a5448e4bc10f033f4f41306e9dbcce983b5a
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-02 01:38:29 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -27,7 +27,6 @@ defmodule CLI do
           {:ok, compressed} = File.read(path)
           decompressed = :zlib.uncompress(compressed)
           [_header, content] = :binary.split(decompressed, <<0>>)
-          IO.write(content)
           IO.binwrite(content)
         else
           IO.puts("NOT")
