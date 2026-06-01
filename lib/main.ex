3e5bbdd060866ec89f41c0ce26e84f98b5f928e0
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-01 22:27:28 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -3,19 +3,18 @@ defmodule CLI do
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts(:stderr, "Logs from your program will appear here!")
 
-    # TODO: Uncomment the code below to pass the first stage
-    #
-    # command = List.first(args)
-    # case command do
-    #   "init" ->
-    #     File.mkdir!(".git")
-    #     File.mkdir!(".git/objects")
-    #     File.mkdir!(".git/refs")
-    #     File.write!(".git/HEAD", "ref: refs/heads/main\n")
-    #     IO.puts("Initialized git directory")
-    #
-    #   _ ->
-    #     raise "Unknown command #{command}"
-    # end
+    command = List.first(args)
+
+    case command do
+      "init" ->
+        File.mkdir!(".git")
+        File.mkdir!(".git/objects")
+        File.mkdir!(".git/refs")
+        File.write!(".git/HEAD", "ref: refs/heads/main\n")
+        IO.puts("Initialized git directory")
+
+      _ ->
+        raise "Unknown command #{command}"
+    end
   end
 end
