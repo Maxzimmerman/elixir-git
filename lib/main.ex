cc7d43c6e651b451569db890089c403ba2a92f7a
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-02 00:31:07 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -16,6 +16,7 @@ defmodule CLI do
       "cat-file" ->
         [_, _, hash] = System.argv()
         IO.inspect(hash)
+        IO.inspect(Path.join(".git/objects/", hash))
 
         if File.exists?(Path.join(".git/objects/", hash)) do
           {:ok, content} = File.read(Path.join(".git/objects/", hash))
