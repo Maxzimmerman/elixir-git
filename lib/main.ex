1e993661c424ee9fbf4610a883ec5566bd7f69eb
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-02 18:22:23 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -29,7 +29,13 @@ defmodule CLI do
         end
 
       "hash-object" ->
-        IO.inspect(System.argv())
+        [_, _, path] = System.argv()
+        content = File.read!(path)
+        IO.inspect(content)
+        header = "blob #{byte_size(content)}\0"
+        store = header <> content
+
+        sha = :crypto.hash(:sha, store) |> Base.encode16(case: :lower)
 
       _ ->
         raise "Unknown command #{command}"
