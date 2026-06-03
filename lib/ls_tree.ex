56cbba79c8d1b7efb3d904f9e97fc4fe8e7ef236
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-03 19:38:12 +0800

    codecrafters submit [skip ci]

--- lib/ls_tree.ex
+++ lib/ls_tree.ex
@@ -8,7 +8,7 @@ defmodule Commands.LsTree do
     decompressed = :zlib.uncompress(compressed_tree_content)
     [_head, content] = :binary.split(decompressed, <<0>>)
     names = decode_file_name(content, [])
-    IO.write(Enum.join(names, "\n") <> "\n")
+    IO.puts(Enum.join(names, "\n") <> "\n")
   end
 
   defp decode_file_name(content, names) when bit_size(content) > 0 do
