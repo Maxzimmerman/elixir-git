6a0f15a59bbe8e21999940db2d4b805d1881b44e
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-03 19:00:28 +0800

    codecrafters submit [skip ci]

--- lib/ls_tree.ex
+++ lib/ls_tree.ex
@@ -5,7 +5,9 @@ defmodule Commands.LsTree do
     [_, _, tree_hash] = System.argv()
     <<dir::binary-size(2), file_hash::binary>> = tree_hash
     {:ok, compressed_tree_content} = File.read(".git/objects/#{dir}/#{file_hash}")
-    decode_file_name(compressed_tree_content)
+    decompressed = :zlib.uncompress(compressed_tree_content)
+    [_head, content] = :binary.split(decompressed, <<0>>)
+    decode_file_name(:zlib.uncompress(compressed_tree_content))
   end
 
   defp decode_file_name(<<tree::binary-size(4), size::binary-size(2), rest::binary>>) do
