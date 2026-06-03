5b1114a37f96a3d0f9df321fa7269d47b76353e4
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-03 20:24:31 +0800

    codecrafters submit [skip ci]

--- lib/write_tree.ex
+++ lib/write_tree.ex
@@ -3,8 +3,11 @@ defmodule Commands.WriteTree do
 
   def execute do
     {:ok, files} = File.ls(".")
+    IO.inspect(files, label: "ALL")
     dirs = Enum.filter(files, &(File.dir?(&1) and &1 != ".git"))
     files = Enum.reject(files, &File.dir?(&1))
+    IO.inspect(dirs, label: "Dirs")
+    IO.inspect(files, label: "Files")
     file_hashes = build_blobs(files, [])
     dir_hashes = build_trees(dirs, [])
   end
